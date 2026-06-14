d3d409205f855ad0e021cb642f5898a126b46d83
ochanine-svg <ochanine@gmail.com>
2026-06-14 18:03:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.latex(r"x_{n+1}=\frac12\left(x_n+\frac{a}{x_n}\right)")
 st.write("The sequence should approach $\\sqrt{a}$.")
 
 
-def fraction_is_short_enough(frac, max_chars=38):
+def fraction_is_short_enough(frac, max_chars=42):
     return len(str(frac)) <= max_chars
 
 def parse_a(a_string):
