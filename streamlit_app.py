cf95849b17c6962fe24e28b0abcd81f76e6155c5
ochanine-svg <ochanine@gmail.com>
2026-06-15 14:35:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ number_of_iterations = st.slider("Number of iterations", 1, 50, 5)
 st.markdown(
     """
     <p style="font-size:20px; font-weight:bold;">
-    Results appear below. You can adjust the number of iterations for more rows or results.
+    Results appear below. You can adjust the adjust the slider for more rows of results.
     </p>
     """,
     unsafe_allow_html=True,
