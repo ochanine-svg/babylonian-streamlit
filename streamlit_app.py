ae65b603d7457ab420e2d60f307e5c6579462b5d
ochanine-svg <ochanine@gmail.com>
2026-06-15 15:19:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ st.markdown(
 st.markdown(
     """
     <p style="font-size:18px;">
-    Try both a decent guess for the square root and a terrible guess to see fast convergence. You may have to adjust the slider to see convergence.
+    Try both a decent guess for the square root and a terrible guess to explore the fast convergence.
     </p>
     """,
     unsafe_allow_html=True,
