a517f0f321765cc6f1a2281bab92dc6c8ae5c7b9
ochanine-svg <ochanine@gmail.com>
2026-06-15 14:46:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,6 +64,15 @@ html, body, [class*="css"] {
 </style>
 """, unsafe_allow_html=True)
 
+st.markdown(
+    """
+    <p style="margin-top:-4px;">
+    This app repeatedly updates the value of <i>x</i> using
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
+
 st.write("This app computes successive updates of the value of $x$ using")
 
 st.latex(r"\frac12\left(x+\frac{a}{x}\right)")
