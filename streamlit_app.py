c97e090c9f2d301dcd8adafb3334d6d84710fc6d
ochanine-svg <ochanine@gmail.com>
2026-06-15 09:33:25 -0500

    Increase width of 'decimal approximation' column

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,7 +178,7 @@ st.dataframe(
         "n": st.column_config.NumberColumn("n", width=20),
         "decimal approximation": st.column_config.TextColumn(
             "decimal approximation",
-            width=80,
+            width=110,
         ),
         "exact value": st.column_config.TextColumn(
             "exact value",
