b24efe07438f6056d573b1e4dd7ee38afe07cf35
ochanine-svg <ochanine@gmail.com>
2026-06-17 09:18:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ html, body, [class*="css"] {
 </style>
 """, unsafe_allow_html=True)
 
-
+st.write("Start with an initial guess for the square root of $a$, which will be stored in a variable $x$")
 st.write("This app computes successive updates of the value of $x$ using")
 
 st.latex(r"\frac12\left(x+\frac{a}{x}\right)")
@@ -86,7 +86,7 @@ st.write("The value of $x$ converges fast to $\sqrt{a}$.")
 a_string = st.text_input("Enter a positive number $a$. You can enter a decimal, whole number or fraction.", value="5")
 
 x0_string = st.text_input(
-    "Enter a positive initial guess for the square root of $a$. This is the initial value of $x$. You can enter a decimal, whole number or fraction.",
+    "Enter a positive initial guess for the square root of $a$. You can enter a decimal, whole number or fraction.",
     value="100"
 )
 
