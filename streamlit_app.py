cced697228b568a781a9205fe6ff896ba55a50d4
ochanine-svg <ochanine@gmail.com>
2026-06-17 20:03:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,37 +183,18 @@ def parse_x0(x0_string, force_float_mode):
 
 def compute_iterations(a, x, use_fractions, number_of_iterations):
     rows = []
-    current_mode = use_fractions
-    switched_from_fraction_to_decimal = False
 
-    if use_fractions:
-        rows.append({"x": f"x0 = {x}"})
-    else:
-        rows.append({"x": f"x0 = {float(x):.14f}"})
+    rows.append({"x": f"x0 = {float(x):.14f}"})
 
     for k in range(1, number_of_iterations):
-        if current_mode:
+        if use_fractions:
             x = Fraction(1, 2) * (x + a / x)
-
-            if fraction_is_short_enough(x):
-                rows.append({"x": f"x{k} = {x}"})
-            else:
-                rows.append({"x": f"x{k} ≈ {float(x):.14f}"})
-                x = float(x)
-                a = float(a)
-                current_mode = False
-                switched_from_fraction_to_decimal = True
-
         else:
             x = 0.5 * (x + a / x)
 
-            if switched_from_fraction_to_decimal:
-                rows.append({"x": f"x{k} ≈ {x:.14f}"})
-            else:
-                rows.append({"x": f"x{k} = {x:.14f}"})
+        rows.append({"x": f"x{k} = {float(x):.14f}"})
 
     return rows
-
 
 a, a_can_use_fractions, a_error = parse_a(a_string)
 
